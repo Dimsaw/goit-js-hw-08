f725e935ab25607f056d4ba855b3747171fd1396
Dima Savkov <dimsaw85@gmail.com>
2021-11-23 11:43:07 +0200

    work1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const allPhotos = document.querySelector('.gallery');
+const galleryPhotos = createPhotosGalerry(galleryItems);
+
+allPhotos.insertAdjacentHTML('afterbegin', galleryPhotos);
+
+function createPhotosGalerry(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>
+</li>
+        `;
+    })
+    .join('');
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime(evt) {
+  {
+    duration: 61.857;
+    percent: 0.049;
+    seconds: 3.034;
+  }
+  localStorage.setItem(LOCALSTORAGE_KEY, evt.seconds);
+}
+
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function (seconds) {
+    seconds = localStorage.getItem(LOCALSTORAGE_KEY);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred;
+        break;
+    }
+  });
