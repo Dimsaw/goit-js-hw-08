d3adf890c2cb19d7289ae56dcc0f16e91b1cf022
Dima Savkov <dimsaw85@gmail.com>
2021-11-24 12:25:48 +0200

    work3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form ');
+const textarea = document.querySelector('textarea');
+const input = document.querySelector('input');
+
+form.addEventListener('input', throttle(areaOfInputs, 500));
+form.addEventListener('submit', submitForm);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+populateTextarea();
+const formData = {};
+
+function submitForm(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(formData);
+}
+
+function areaOfInputs(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+  if (parsedMessage) {
+    textarea.value = parsedMessage.message;
+    input.value = parsedMessage.email;
+  }
+}
