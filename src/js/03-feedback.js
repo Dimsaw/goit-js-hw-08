0adb6f72d6f65e0b640e657788070ab588c764b8
Dima Savkov <dimsaw85@gmail.com>
2021-11-27 20:56:22 +0200

    fixed3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,24 +4,30 @@ const form = document.querySelector('form ');
 const textarea = document.querySelector('textarea');
 const input = document.querySelector('input');
 
-form.addEventListener('input', throttle(areaOfInputs, 500));
-form.addEventListener('submit', submitForm);
-
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 populateTextarea();
-const formData = {};
+
+form.addEventListener('input', throttle(areaOfInputs, 500));
+form.addEventListener('submit', submitForm);
 
 function submitForm(evt) {
   evt.preventDefault();
 
-  evt.currentTarget.reset();
+  evt.target.reset();
+  console.log('LOCALSTORAGE_KEY', JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
   localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(formData);
 }
+const formData = { email: input.value, message: textarea.value };
 
 function areaOfInputs(evt) {
   formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({
+      email: input.value,
+      message: textarea.value,
+    }),
+  );
 }
 
 function populateTextarea() {
@@ -30,5 +36,6 @@ function populateTextarea() {
   if (parsedMessage) {
     textarea.value = parsedMessage.message;
     input.value = parsedMessage.email;
+    console.log(parsedMessage);
   }
 }
